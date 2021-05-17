fdd821abc37a162296e330fb80dba30e7e32b787
Martín Cervantes <martin.cervantes@wbox.mx>
2021-05-16 22:54:39 -0500

    add db

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,12 @@ process.env.DB_PASS = 'declaraf_qa';
 process.env.DB_NAME = 'declaraf_qa';
 process.env.DB_DIALECT = 'mysql';
 
+process.env.DB_HOST = 'localhost';
+process.env.DB_USER = 'root';
+process.env.DB_PASS = 'admin';
+process.env.DB_NAME = 'db_lawyers';
+process.env.DB_DIALECT = 'mysql';
+
 // process.env.SEED = 'TmBGoczbeaVDEqCrURVLQPmSVFFLmBqDO0Ey8IIxfTsbZ8SKAGhn5ADL2Yi1AWgy';
 // process.env.EXPIRATION_TOKEN = 86400000;
 
